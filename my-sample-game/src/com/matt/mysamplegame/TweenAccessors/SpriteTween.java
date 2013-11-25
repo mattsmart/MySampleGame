09f4b0a5fae2a68cf0c80db787e809bbe81cd3f0
mattsmart <mattsmart@hotmail.ca>
2013-11-24 21:55:02 -0500

    17

---
+++ my-sample-game/src/com/matt/mysamplegame/TweenAccessors/SpriteTween.java
@@ -0,0 +1,9 @@
+package com.matt.mysamplegame.TweenAccessors;
+
+import aurelienribon.tweenengine.TweenAccessor;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class SpriteTween implements TweenAccessor<Sprite> {
+
+}
